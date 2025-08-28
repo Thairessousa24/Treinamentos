a6b47e61c745c63f8462cf6cae714c9f3347b3ab
Thaires Sousa <thaires.rodrigues@aluno.ce.gov.br>
2025-08-28 14:49:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let idade = parseInt(prompt("Digite sua idade"))
-let ingresso = prompt("Você possue ingresso? [S/N] ").toLowerCase()
-
-if (ingresso === "s") {
-    ingresso = true
-    if (idade >= 18 && ingresso == true) {
-        alert("Você pode entrar! Pois possui ingresso!")
-    } else {
-        alert("Você não pode entrar, pois não possui idade suficiente")
-    }
-
-} else if (ingresso === "n") {
-    ingresso = false
-    alert("Você não possue ingresso")
-} else {
-    alert("Você digitou algo errado. Fim do Programa.")
-}
+console.log = ("Olá mundo")
